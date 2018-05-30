f8e69bcab8b7d3d5da3e7bc825d312177f526234
fifidwiariani <36693653+fifidwiariani@users.noreply.github.com>
2018-05-30 16:30:48 +0700

    update koneksi 1

--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,7 @@
 class Koneksi {
     private $server = "localhost";
     private $username = "id4905138_dev"; //root
-    private $password = "ariani13"; //""
+    private $password = "12345"; //""
     private $db = "id4905138_stok_barang";
     function getKoneksi() {
         return new mysqli($this->server, $this->username,
