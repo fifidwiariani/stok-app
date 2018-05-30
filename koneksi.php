74a7f6bf9864cf348bedc8172a15c7d50411ba0c
fifidwiariani <36693653+fifidwiariani@users.noreply.github.com>
2018-05-30 16:00:31 +0700

    github

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,9 @@
 <?php
 class Koneksi {
     private $server = "localhost";
-    private $username = "root"; //root
+    private $username = "id4905138_dev"; //root
     private $password = ""; //""
-    private $db = "php_kelas_c";
+    private $db = "id4905138_stok_barang";
     function getKoneksi() {
         return new mysqli($this->server, $this->username,
         $this->password, $this->db);
